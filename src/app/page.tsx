f17723c2c195780d9b5b285b2fee0a5d62593ca9
Amanda <inagakiamanda8@gmail.com>
2026-03-20 15:03:19 -0300

    Agora é com a alicia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ export default function Home() {
         <section>
           <FaStar color="orange" className="star" size={20}/>
         </section>
-        <article>
-
+        <article> 
+          <h1>How did we do?</h1>
+          <p>Please let us know how we did with your support request. All feedback is appreciated! to help us improve our offering!</p>
         </article>
       </div>
     </main>
